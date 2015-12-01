fa9508a077165920a3e4519aa384e8abca18d2f0
jmontroy90 <jmontroy90@gmail.com>
2015-12-01 01:26:41 -0500

    adding wordcloud, couple other tweaks

--- hillarydash/app/__init__.py
+++ hillarydash/app/__init__.py
@@ -2,17 +2,22 @@ from __future__ import division
 import random
 import StringIO
 from datetime import datetime
+import re
+from os import path
 
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image
 
 from flask import Flask, g, session, make_response
 from flask import render_template, flash, redirect, url_for
 from flask import request, send_file
-from .forms import PersonTopicForm
+from .forms import PersonTopicForm, WordcloudForm
 from flask.ext.mysql import MySQL
 from flask.ext.cache import Cache
 
@@ -32,7 +37,7 @@ def CountsByKeyword(df, col, person, topics, StartDate = '2009-01-01', EndDate =
     'By' parameter controls which field you're getting counts by.
     Big return says: return a dictionary via comprehension for lists, or just a dict for one value
     
-    Ultimately this should return a pandas DF, but pandas is finicky.
+    This is not a good function. Change it to return one dataframe for all people/topics.
     """
     
     if not isinstance(topics, (str, unicode, list)): 
@@ -48,20 +53,14 @@ def CountsByKeyword(df, col, person, topics, StartDate = '2009-01-01', EndDate =
                 & (df['ExtractedBodyText'].str.contains(topic, case = False))
                 & (df['MetadataDateSent'] > StartDate)
                 & (df['MetadataDateSent'] < EndDate)].count()
-            for topic in topics} 
-        if isinstance(topics, list) 
-        else {topics: df[col].loc[
-                (df[col].str.contains(person, case = False))
-                & (df['ExtractedBodyText'].str.contains(topics, case = False))
-                & (df['MetadataDateSent'] > StartDate)
-                & (df['MetadataDateSent'] < EndDate)].count()}
+            for topic in topics}
     )
 
 # @cache.cached(timeout=600, key_prefix='emails')
 def getEmails():
 	con = mysql.connect()
 	cur = con.cursor()
-	sql = """SELECT * FROM Emails"""
+	sql = """SELECT * FROM EmailsC"""
 
 	cur.execute(sql)
 
@@ -81,7 +80,6 @@ def buildCounterDF(personlist, topiclist):
 	PersonThing = list()
 	PersonTopic = pd.DataFrame()
 
-	personlist = personlist.split(',')
 	topiclist = topiclist.split(',')
 
 	for person in personlist:
@@ -100,19 +98,71 @@ def buildCounterDF(personlist, topiclist):
 
 		PersonTopic = PersonTopic.append(tdf)
 
-	remap = {'^Abedin, Huma$': 'Huma Abedin', 
-	        '^sbwhoeop$': 'Sidney Blumenthal', 
-	        '^Sullivan, Jacob J$': 'Jacob Sullivan', 
-	        '^H$': 'Hillary Clinton', 
-	        '^Mills, Cheryl D$': 'Cheryl Mills'}
-
-	PersonTopic.Person.replace(remap, inplace=True)
-
 	return PersonTopic
+
+def rmNonAlpha(texts):  
+    """
+    Remove non-alphabetic characters (roughly)
+    """
+    
+    if isinstance(texts, list):
+        ctext = [re.sub(r'\s+', ' ', ctext) for ctext in [re.sub(r'[[\]()<>{}!:,;-_|\."\'\\]', '', text) for text in texts]]
+    
+    elif isinstance(texts, (str, unicode)):
+        ctext = re.sub(r'[(){}<>,\.!?;:\'"/\\\_|]', '', texts)
+    
+    return ctext
+
+def rmBoring(texts):
+    """
+    Remove boring stuff.
+    Warning: strong assumptions ahead...but we gotta do some chopping.
+    """
+    
+    # overhead stuff
+    ctext = re.sub(r'^From .*\n', '', texts, flags=re.MULTILINE)
+    ctext = re.sub(r'^To .*\n', '', ctext, flags=re.MULTILINE)
+    ctext = re.sub(r'^Case No .*\n', '', ctext, flags=re.MULTILINE)
+    ctext = re.sub(r'^Sent .*\n', '', ctext, flags=re.MULTILINE)
+    ctext = re.sub(r'^Doc No .*\n', '', ctext, flags=re.MULTILINE)
+    ctext = re.sub(r'^Subject .*\n', '', ctext, flags=re.MULTILINE)
+    
+    # other misc
+    ctext = re.sub(r'.*@.*', '', ctext) # emails
+    ctext = re.sub(r'(?i)(monday|tuesday|wednesday|thursday|friday|saturday|sunday).*\d{3,4} [AP]M\n', '', ctext, flags = re.MULTILINE) # timestamps
+    ctext = re.sub(r'Fw .*\n', '', ctext, flags = re.MULTILINE) # forward line
+    ctext = re.sub(r'Cc .*\n', '', ctext, flags = re.MULTILINE) # Cc line
+    ctext = re.sub(r'B[56(7C)]', '', ctext)
+    
+    # house benghazi committee stuff
+    ctext = re.sub(r'Date 05132015.*\n', '', ctext, flags = re.MULTILINE) 
+    ctext = re.sub(r'STATE DEPT  .*\n', '', ctext, flags = re.MULTILINE)
+    ctext = re.sub(r'SUBJECT TO AGREEMENT.*\n', '', ctext, flags = re.MULTILINE)
+    ctext = re.sub(r'US Department of State.*\n', '', ctext, flags = re.MULTILINE)
+
+    return re.sub(r'\s+', ' ', ctext).lower()
+
+def GetTextRange(df, person, dateFrom = '2008-05-01', dateTo = '2015-05-13'):
+    """
+    Returns string of all email text from particular date range for particular person
+    
+    dateFrom and dateTo must both be in formt 'YYYY-MM-DD'.
+    """
+    
+    stoplist = set('for a of the and to in on from'.split())
+
+    emails = df.loc[
+        (df.MetadataDateSent > datetime.strptime(dateFrom, '%Y-%m-%d').date())
+        & (df.MetadataDateSent < datetime.strptime(dateTo, '%Y-%m-%d').date())
+        & (df.MetadataFrom.str.contains(person)), 
+        'ExtractedBodyText'] \
+    .values.tolist()
+    
+    return ' '.join([word for word in emails if word not in stoplist])
 
 # get emails globally?
 Emails = getEmails()
-Emails.MetadataDateSent = Emails.MetadataDateSent.astype('datetime64[ns]')
+Emails.MetadataDateSent = pd.to_datetime(Emails.MetadataDateSent)
 
 ###############
 # BEGIN VIEWS #
@@ -128,53 +178,78 @@ def index():
 def counter():
     form = PersonTopicForm(request.form)
 
-    personlist = request.args.get('personlist')
+    personlist = request.args.getlist('personlist')
     topiclist = request.args.get('topiclist')
-    
+
     if request.method == 'POST' and form.validate():        
         return redirect(url_for('counter'))
 
     return render_template('counter.html', 
                             form = form,
-                            personlist = personlist,
+                            personlist = ','.join(personlist),
                             topiclist = topiclist)   
 
 
+@app.route('/wordcloud', methods=['GET', 'POST'])
+def wordcloud():
+    form = WordcloudForm(request.form)
 
-@app.route('/fig/<personlist>/<topiclist>/plot.png')
-def plot(personlist, topiclist):
+    person = request.args.get('person')
 
-	PersonTopic = buildCounterDF(personlist, topiclist)
+    if request.method == 'POST' and form.validate():        
+        return redirect(url_for('wordcloud'))
 
-	plt.clf()
+    return render_template('wordcloud.html', 
+                            form = form,
+                            person = person)   
 
-	plt.rcParams['figure.figsize'] = [10,5]
-	sns.barplot(x='Person', y = 'count', hue = 'Topic', data=PersonTopic)
-	plt.ylabel('Number of emails with mention')
-	plt.xlabel('Sender of email')
-	
-	fig = plt.gcf()
-	img = StringIO.StringIO()
-	fig.savefig(img)
-	img.seek(0)
+@app.route('/fig/<personlist>/<topiclist>/ptplot.png')
+def ptplot(personlist, topiclist):
 
-	return send_file(img, mimetype='image/png')
+    PersonTopic = buildCounterDF(personlist.split(','), topiclist)
 
-	# canvas = FigureCanvas(fig)
-	# output = StringIO.StringIO()
-	# canvas.print_png(output)
-	# response = make_response(output.getvalue())
-	# response.mimetype = 'image/png'
-	# return response
+    # scale = len(personlist.join(','))
 
-# @app.route('/fig/<personlist>/<topiclist>')
-# def fig(personlist, topiclist):
-#     fig = draw_polygons(cropzonekey)
-#     img = StringIO()
-#     fig.savefig(img)
-#     img.seek(0)
-#     return send_file(img, mimetype='image/png')
+    plt.clf()
+
+    sns.barplot(x='Person', y = 'count', hue = 'Topic', data=PersonTopic)
+    plt.ylabel('Number of emails with mention')
+    plt.xlabel('Sender of email')
+
+    fig = plt.gcf()
+    img = StringIO.StringIO()
+    fig.savefig(img)
+    img.seek(0)
+
+    return send_file(img, mimetype='image/png')
+
+@app.route('/fig/<person>/cloudplot.png')
+def cloudplot(person):
+
+    text = GetTextRange(Emails, person)
+    text = rmBoring(rmNonAlpha(text)).decode('ascii', 'ignore')
+
+    plt.clf()
+
+    d = path.dirname(path.abspath(__file__))
+
+    hilcolor = np.array(Image.open(path.join(d, "static/img/hillarylogo.jpg")))
+
+    wc = WordCloud(background_color="white", max_words=150, mask=hilcolor,
+               stopwords=STOPWORDS.add("said"),
+               max_font_size=80, random_state=42,
+               relative_scaling = 0.5)
 
 
+    wc.generate(text)
+    image_colors = ImageColorGenerator(hilcolor)
 
+    plt.imshow(wc.recolor(color_func=image_colors))
+    plt.axis("off")
 
+    fig = plt.gcf()
+    img = StringIO.StringIO()
+    fig.savefig(img)
+    img.seek(0)
+
+    return send_file(img, mimetype='image/png')
--- hillarydash/app/forms.py
+++ hillarydash/app/forms.py
@@ -1,7 +1,36 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, TextAreaField
+from wtforms import StringField, BooleanField, TextAreaField, SelectMultipleField, SelectField
 from wtforms.validators import DataRequired
 
+fullpersonlist = [('Hillary Clinton', 'Hillary Clinton'), 
+    					('Sidney Blumenthal', 'Sidney Blumenthal'), 
+    					('Huma Abedin', 'Huma Abedin'),
+    					('Jake Sullivan', 'Jake Sullivan'),
+    					('Cheryl Mills', 'Cheryl Mills'),
+    					('Lauren Jiloty', 'Lauren Jiloty'),
+    					('Philippe Reines', 'Philippe Reines'),
+    					('Lona Valmoro', 'Lona Valmoro'),
+    					('Anne Marie Slaughter', 'Anne Marie Slaughter'),
+    					('Richard Verma', 'Richard Verma'),
+    					('Melanne Verveer', 'Melanne Verveer'),
+    					('Lissa Muscatine', 'Lissa Muscatine'),
+    					('Judith McHale', 'Judith McHale'),
+    					('Strobe Talbott', 'Strobe Talbott'),
+    					('Betsy Ebeling', 'Betsy Ebeling'),
+    					('Monica Hanley', 'Monica Hanley'),
+    					('Robert Russo', 'Robert Russo'),
+    					('Kris Balderston', 'Kris Balderston'),
+    					('Cherie Blair', 'Cherie Blair')
+    				]
+
 class PersonTopicForm(Form):
-    personlist = StringField('personlist', validators=[DataRequired()])
+    personlist = SelectMultipleField('personlist', 
+    				choices = fullpersonlist, 
+    				validators=[DataRequired()])
+
     topiclist = StringField('topiclist', validators=[DataRequired()])
+
+class WordcloudForm(Form):
+	person = SelectField('person',
+					choices = fullpersonlist,
+					validators = [DataRequired()])
